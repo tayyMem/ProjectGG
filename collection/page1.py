6a43cfcb625fd2e08cefc436a725beca7e89e0b2
tayyMem <invaliduzer@gmail.com>
2025-03-20 19:30:06 -0700

    Update page1.py

--- collection/page1.py
+++ collection/page1.py
@@ -12,7 +12,7 @@ st.set_page_config(layout="wide")
 nav = get_nav_from_toml(".streamlit/sidebar.toml")
 
 # Display your logo (adjust the path if necessary)
-st.logo("naruto.jpg")
+st.logo("naruto1.jpg")
 
 # Initialize navigation using the st_pages library
 pg = st.navigation(nav)

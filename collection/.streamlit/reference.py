fc5d9daec6dea9d8fe8071d1d85ad040d6d05fc6
tayyMem <invaliduzer@gmail.com>
2025-03-20 20:19:46 -0700

    Add files via upload
    updated about reference page in sidebar.toml
    reference.py is added for new page as a reference list

---
+++ collection/.streamlit/reference.py
@@ -0,0 +1,11 @@
+import streamlit as st
+st.divider() 
+st.caption("Our appreciation and credits go to below sites in order to build this project:")
+#st.subheader("We are Group2 from Simbolo AI Batch 20's. Our project is to deliver assistance in B2 level students' grammar and spelling mistakes correction.")
+st.subheader(":orange[**For Design:**] ")
+st.caption("https://docs.streamlit.io/develop/api-reference/text")
+st.caption("https://docs.streamlit.io/develop/api-reference/text")
+
+st.subheader(":blue[**For Model:**] ")
+st.caption("https://docs.streamlit.io/develop/api-reference/text")
+st.caption("https://docs.streamlit.io/develop/api-reference/text")

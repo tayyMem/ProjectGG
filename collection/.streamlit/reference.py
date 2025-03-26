fe2ef28efa4794dfacef6bf42ca60905b9a7f2cc
tayyMem <invaliduzer@gmail.com>
2025-03-25 18:28:56 -0700

    Add files via upload
    more ref

--- collection/.streamlit/reference.py
+++ collection/.streamlit/reference.py
@@ -14,9 +14,17 @@ st.subheader(":blue[**Weight Decay:**] ")
 st.caption("https://www.kaggle.com/code/zalcode/fine-tuning-a-llm-for-grammar-correction")
 st.caption("https://paperswithcode.com/method/weight-decay")
 
-st.subheader(":blue[**Num Beam:**] ")
+st.subheader(":cyan[**Num Beam:**] ")
 st.caption("https://huggingface.co/blog/how-to-generate")
 
 st.subheader(":yellow[**Bleu Evaluation**] ")
 st.caption("https://www.geeksforgeeks.org/nlp-bleu-score-for-evaluating-neural-machine-translation-python/")
+st.caption("chrome-extension://efaidnbmnnnibpcajpcglclefindmkaj/https://aclanthology.org/P02-1040.pdf")
 
+st.subheader(":orange[**CHRF**] ")
+st.caption("https://huggingface.co/spaces/evaluate-metric/chrf")
+
+
+st.subheader(":green[**rouge**] ")
+st.caption("https://medium.com/@sthanikamsanthosh1994/understanding-bleu-and-rouge-score-for-nlp-evaluation-1ab334ecadcb#:~:text=pn%20is%20the%20precision%20of,a%20BLEU%20score%20of%200.")
+st.caption("https://medium.com/@anyuanay/fine-tuning-the-pre-trained-t5-small-model-in-hugging-face-for-text-summarization-3d48eb3c4360")
